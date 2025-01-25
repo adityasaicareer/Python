40c7d0c9b10a46d7d92750d8ab0afba5b6730e26
Adityasai <adityasaicareer@gmail.com>
2025-01-25 21:22:15 +0530

    first commit

---
+++ Revision/datatypes.py
@@ -0,0 +1,13 @@
+print(type(1))
+print(type(1.0))
+print(type(-1))
+print(type(-1.0))
+print(type('aditua'))
+print(type('a'))
+print(type(1000000000000))
+# there is no difference between charecter and string in python
+
+print(type(4E2)) 
+# the above was an float datatype
+# 4x10 power2
+
