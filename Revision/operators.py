9860c34636f1fc7d7d7532bdbd7cbdc295080b3a
Adityasai <adityasaicareer@gmail.com>
2025-01-25 21:29:08 +0530

    operators

---
+++ Revision/operators.py
@@ -0,0 +1,35 @@
+# arithmetic operators
+
+
+print(10+8)
+print(10-8)
+print(10*8)
+print(10/8)
+# floor division
+
+print(10//8)
+
+# remainder
+
+print(10%8)
+
+print(pow(10,3))
+# absolute function
+print(abs(-100))
+# round function
+print(round(5.43))
+print(round(5.49))
+print(round(5.79))
+
+
+# convert to binary
+
+print(bin(10))
+print(type(bin(10)))
+print(bin(10)[2:])
+
+# convert to hexadecimal
+
+print(hex(100))
+print(type(hex(100)))
+print(hex(100)[2:])
