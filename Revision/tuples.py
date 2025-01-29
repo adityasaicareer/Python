01bfdeb6b5a50d1296f207668dc9fcc6637f8283
Adityasai <adityasaicareer@gmail.com>
2025-01-29 22:31:08 +0530

    tupples basics

---
+++ Revision/tuples.py
@@ -0,0 +1,16 @@
+mytuples=('apple','grapes','mango','grapes')
+print(mytuples)
+
+print(len(mytuples))
+print(mytuples[2])
+print(mytuples[-1])
+# immutability
+
+# mytuples[1]='donuts'
+# the above statements will yell the error
+
+# methods
+
+print(mytuples.index('grapes'))
+print(mytuples.count('grapes'))
+
