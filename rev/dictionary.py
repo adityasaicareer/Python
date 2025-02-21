5905f41c80a7b4579bb2037f00614238c9903d33
Adityasai <adityasaicareer@gmail.com>
2025-02-21 10:13:21 +0530

    this is dictionary

---
+++ rev/dictionary.py
@@ -0,0 +1,30 @@
+dic={'name':'Aditya','age':25,'magic_power':False}
+print(dic)
+
+## retirival
+
+print(dic['name'])
+
+print(len(dic))
+print(list(dic))
+print(dic.keys())
+print(dic.values())
+
+print(dic.items())
+
+dic['weight']=90
+
+print(dic)
+## get method
+
+print(dic.get('name'))
+del dic['magic_power']
+print(dic)
+
+dic.update({'name':"Aditya Sai"})
+print(dic)
+
+newdict=dict(zip(['name','age'],['aditya',20]))
+print(newdict)
+
+
